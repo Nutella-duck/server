2f1170f866f7e98f03c9642cc4f6d47628d3785f
오송미 <songmi.ohh@gmail.com>
2021-01-16 09:03:25 +0900

    로그인 middleware 추가

---
+++ applications/auth/authorizationMW.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const MY_SECRET_KEY = process.env.SECRET_KEY;
+
+const verifyToken = (req, res, next) => {
+    const clientToken = req.headers['x-access-token'];
+
+    if(!clientToken){
+        return res.status(400).end("Please Login");
+    }
+
+    try {
+        const decoded = jwt.verify(clientToken, MY_SECRET_KEY);
+
+        if(decoded){
+            res.locals.userId = decoded.usr;
+            // usr가 없을 때도?
+            next();
+        } else {
+            res.status(401).json({ error: "unauthorized"});
+        }
+    } catch (err) {
+        res.status(401).json({ error: "token expired"});
+    }
+};
+
+module.exports = verifyToken;
