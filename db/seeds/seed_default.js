693fba945469a597f09518ef17c74e5702c4aaf3
오송미 <songmi.ohh@gmail.com>
2021-01-19 18:03:59 +0900

    seed 수정

--- db/seeds/seed_default.js
+++ db/seeds/seed_default.js
@@ -1,3 +1,5 @@
+const { JsonWebTokenError } = require("jsonwebtoken");
+
 exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex("user")
@@ -53,9 +55,15 @@ exports.seed = function (knex) {
           })
           .then(() => {
             return knex("hpoConfig").insert([ //bestResult 없음
-              { hpoProjectId: 1, method: 2, config: JSON.stringify({"epoch": {"scope": [1, 10]}, "learning_rate": {"value": [0.1, 0.05, 0.01]}}), bestParameter: JSON.stringify({"epoch": 1, "learning_rate": 0.05}) },
-              { hpoProjectId: 2, method: 2, config: JSON.stringify({"units": {"scope":[64,1024]}, "dropout1": {"scope":[0.25, 0.75]},
-               "optimizer": {"value":['rmsprop', 'adadelta', 'adam']}, "batch_size": {"value":[128, 512]}}) },
+              { hpoProjectId: 1, method: 2, 
+                config: JSON.stringify({"epoch": {"scope": [1, 10]}, "learning_rate": {"value": [0.1, 0.05, 0.01]}}),
+                bestParameter: JSON.stringify({"epoch": 1, "learning_rate": 0.05}),
+                bestResult: JSON.stringify({"loss": 1.8}) },
+              { hpoProjectId: 2, method: 2, 
+                config: JSON.stringify({"units": {"scope":[64,1024]}, "dropout1": {"scope":[0.25, 0.75]},
+               "optimizer": {"value":['rmsprop', 'adadelta', 'adam']}, "batch_size": {"value":[128, 512]}}),
+                bestParameter: JSON.stringify({"units": 64, "dropout1": 0.25, "optimizer": "adam", "batch_size": 128}),
+                bestResult: JSON.stringify({"loss": 3.1}) },
             ]);
           })
           .then(() => {
